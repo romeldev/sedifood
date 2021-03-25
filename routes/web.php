46bbc1823cab269025813fb1466ede3542f924fb
romel diaz <root.romel@gmail.com>
2021-03-25 14:03:27 -0500

    fix route Route::fallback arrow

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/test', 'TestController@index');
 
-Route::fallback(fn() => view('spa'));
+Route::fallback(function() {
+    return view('spa');
+});
